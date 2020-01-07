d0da3987b0aef7ba7d438623dbf26f38ab450f88
lupries <lupries@github.com>
2020-01-07 22:31:36 +0100

    corrected TAGNN model

--- models/TAGNN.py
+++ models/TAGNN.py
@@ -1,24 +1,26 @@
 import torch.nn as nn
+from torch.nn import functional as F
+from torchvision import models
 
 from .graphnet import AGNN
-from torchvision import models
 
 class TAGNN(nn.Module):
 
     def __init__(self, loops):
         super(TAGNN, self).__init__()
 
-        self.deeplab = models.segmentation.deeplabv3_resnet50(pretrained=False)
-        self.backbone = self.deeplab.backbone
+        deeplab = models.segmentation.deeplabv3_resnet50(pretrained=False)
+        self.backbone = deeplab.backbone
         self.graph = AGNN(loops=loops, channels=2048)
         self.readout = models.segmentation.deeplabv3.DeepLabHead(2048, num_classes=1)
 
     def forward(self, x):
 
+        input_shape = x.shape[-2:]
         features = self.backbone(x)
         x = features['out']
         x = self.graph(x)
         x = self.readout(x)
 
-        return x
+        return F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
 
---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+from .TAGNN import TAGNN
